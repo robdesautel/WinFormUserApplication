a5feae0885c46992173a8d75a3cfbc54b058311d
robdesautel <rob.desautel@gmail.com>
2020-06-20 12:15:26 -0400

    creating new password with salt.

---
+++ UserValidation/PasswordInteraction/CreatePassword.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserValidation.PasswordInteraction
+{
+    internal class CreatePassword : ICreatePassword
+    {
+
+        public string CreateHashPassword(string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string CreateSalt(byte[] salt)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
